ba17f4ef0eb2dcfd57fbd36c76815683325a5701
Juan Pablo Garcia <juampigarcia2014@gmail.com>
2019-10-13 20:27:21 +0000

    Update ej3.py, ej4.py, ej5.py files

--- ej4.py
+++ ej4.py
@@ -1,17 +1,17 @@
-import multiprocessing
-from multiprocessing import Process
 import os
 
-
 def child(num):
-    print("\n#El hijo incrementa el numero en 2.")
-    print("Hijo:", num + 2)  # El Hijo incrementa el numero en 2
+    print("\n#El hijo decrementa el numero en 2.")
+    print("Hijo:", num - 2)  # El Hijo incrementa el numero en 2
+    os._exit(0)
 
-
-if __name__ == "__main__":
+def parent():
     num = int(input("Ingrese numero: "))
-    print("\n#El padre incrementa el numero en 4.")
-    print("Padre:", num + 4)  # El Padre incrementa el numero en 4
-    p = Process(target=child, args=(num,))
-    p.start()
-    p.join()
+    childProc = os.fork()
+    if childProc == 0:
+        child(num)
+    else:
+        print("\n#El padre incrementa el numero en 4.")
+        print("Padre:", num + 4)  # El Padre incrementa el numero en 4
+
+parent()
